837a2f037e7c9a9bd7bdfe9a14261308901e1874
Steven Ko <stevenk8819@gmail.com>
2022-09-08 13:54:43 -0700

    forgot gitignore before

--- src/Blob.java
+++ src/Blob.java
@@ -3,18 +3,29 @@ import java.nio.*;
 import java.nio.file.*;
 import java.nio.file.Path;
 import java.io.*; 
-import java.util.*; 
+import java.util.*;
+import java.util.zip.ZipOutputStream;
 import java.security.*; 
+import java.util.zip.*; 
+import java.nio.*; 
+
 
 
 public class Blob {
 	private String hashed; 
 	private String unhashed; 
+	private String zipped; 
+
 
 
 	public static void main (String [] args) throws IOException {
 		Blob b = new Blob ("test/Stuff.txt"); 
+
+
+
+
 	}
+
 	public Blob (String fileName) throws IOException {
 
 		String ret = "";
@@ -37,15 +48,16 @@ public class Blob {
 		hashed = getSHA1(unhashed);
 		createFBlob();
 
-//		System.out.println("text:" + ret);
-//		System.out.println("encoded:" + hashed);
-//		System.out.println ("BLOB^^"); 
+		//		System.out.println("text:" + ret);
+		//		System.out.println("encoded:" + hashed);
+		//		System.out.println ("BLOB^^"); 
 
 
 		//		read.close(); 
 
 	}
-	
+
+
 	public Blob (String fileName, String Index) throws IOException {
 
 		String ret = "";
@@ -68,14 +80,44 @@ public class Blob {
 		hashed = getSHA1(unhashed);
 		createFIndex();
 
-//		System.out.println("text:" + ret);
-//		System.out.println("encoded:" + hashed);
-//		System.out.println ("BLOB^^"); 
+		//		System.out.println("text:" + ret);
+		//		System.out.println("encoded:" + hashed);
+		//		System.out.println ("BLOB^^"); 
 
 
 		//		read.close(); 
 
 	}
+
+
+	//public void zip() throws IOException {
+	//		
+	//		String filePath = "test/Stuff.txt";
+	//		String zipPath = "test/objects/" + hashed + ".txt";
+	//
+	//		try (ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipPath))) {
+	//		    File fileToZip = new File(filePath);
+	//		    zipOut.putNextEntry(new ZipEntry(fileToZip.getName()));
+	//		    Files.copy(fileToZip.toPath(), zipOut);
+	//		}
+	//		
+	//
+	//	}
+	//	public String getZipped() throws FileNotFoundException {
+	//		String temp = ""; 
+	//		File f1 = new File("test/objects/" + hashed + ".txt");
+	////		PrintWriter pw = new PrintWriter(f1); 
+	////		pw.append("F"); 
+	////		pw.close(); 
+	//		Scanner read = new Scanner(f1);
+	//		while (read.hasNextLine()) {
+	//			temp += read.nextLine();
+	//		}
+	//		read.close(); 
+	//		zipped = temp; 
+	//		return temp; 
+	//	}
+
 
 	private String getSHA1 (String s1){
 		String s = s1;
@@ -99,21 +141,34 @@ public class Blob {
 	}
 
 	public void createFBlob () throws IOException { //with hashed as name
-		
+
+
+		//paths way
+		File f1 = new File ("objects"); 
+//		Path folder = Paths.get(f1.toString());
+//		Path path = Paths.get(hashed + ".txt"); 
+//		Files.writeString(folder, unhashed);
+//		PrintWriter pw = new PrintWriter(folder.toString()); 
+//		pw.close(); 
+
+
+		//pw way
 		File f = new File (hashed + ".txt"); 
-		PrintWriter pw = new PrintWriter("test/objects/" + f); 
+		PrintWriter pw = new PrintWriter("test/objects/" + f); //in test folder
 		pw.append(unhashed); 
 		pw.close(); 
-		
+
 
 	}
 	public void createFIndex () throws IOException { //with hashed as name
-		
+
 		File f = new File (hashed + ".txt"); 
+		//		Path path = Paths.get(hashed + ".txt");
+		//		PrintWriter pw = new PrintWriter(path.toString()); 
 		PrintWriter pw = new PrintWriter("test/objectsIndex/" + f); 
 		pw.append(unhashed); 
 		pw.close(); 
-		
+
 
 	}
 
