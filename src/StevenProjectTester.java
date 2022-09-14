d95f72e7b17a7be4702527549608969a367bccf4
Chuk724 <cmourbrother@gmail.com>
2022-09-14 14:28:44 -0700

    91.5% of code tested - blob creation, Index initialization, add/remove from Index

--- src/StevenProjectTester.java
+++ src/StevenProjectTester.java
@@ -5,7 +5,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -14,7 +13,7 @@ class StevenProjectTester {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		 Path p = Paths.get("test.txt");
+		 Path p = Paths.get("test/test.txt");
 	        try {
 	            Files.writeString(p, "example", StandardCharsets.ISO_8859_1);
 	        } catch (IOException e) {
@@ -23,20 +22,26 @@ class StevenProjectTester {
 	        }
 	}
 
-	/**@AfterAll
+	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-		String shaCode = "c3499c2729730a7f807efb8676a92dcb6f8a3f8f";
-		File removed1 = new File("objects/" + shaCode);
+		/**String shaCode = "c3499c2729730a7f807efb8676a92dcb6f8a3f8f.txt";
+		File removed1 = new File("test/objects/" + shaCode);
 		removed1.delete();
 		File removed2 = new File ("test.txt");
 		removed2.delete();
 		
+		File objectsFolder = new File ("test/objects");
+		objectsFolder.delete();
+		File index = new File("test/Index.txt");
+		index.delete();
+		**/
+		
 	
-	}**/
+	}
 	
 	@Test
 	void testBlobCreation() throws Exception{
-		Blob testBlob = new Blob("test.txt");
+		Blob testBlob = new Blob("test/test.txt");
 		String shaCode = "c3499c2729730a7f807efb8676a92dcb6f8a3f8f.txt";
 		File file = new File("test/objects/" + shaCode);
 		assertTrue(file.exists());
@@ -48,11 +53,20 @@ class StevenProjectTester {
 		Index index = new Index();
 		index.init();
 		
-		File file = new File("index");
+		//File file = new File("Index.txt");
+		Path filePath = Paths.get("test/Index.txt");
+		assertTrue(Files.exists(filePath));
+		
+		Path path = Paths.get("test/objects");
+		assertTrue(Files.exists(path));
+		
+		index.add("test.txt");
+		String shaCode = "c3499c2729730a7f807efb8676a92dcb6f8a3f8f.txt";
+		File file = new File("test/objects/" +shaCode);
 		assertTrue(file.exists());
 		
-		Path path = Paths.get("objects");
-		assertTrue(Files.exists(path));
+		index.remove("c3499c2729730a7f807efb8676a92dcb6f8a3f8f");
+		assertTrue(file.exists());
 	}
 
 }
